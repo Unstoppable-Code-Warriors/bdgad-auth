e4211b1feec7e8e3082e0188874fc2172cb81649
ThangNgo123 <ngosythang123456@gmail.com>
2025-06-17 18:14:09 +0700

    refactor: login admin ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,21 @@
 import { LoginForm } from "./_components/login-form"
+import Image from "next/image"
 
 export default function LoginPage() {
 	return (
 		<div className="min-h-screen flex">
 			{/* Left side - Blue gradient background */}
-			<div className="w-3/5 bg-gradient-to-br from-blue-600 to-blue-800 flex items-center justify-center">
+			<div className="w-3/5 bg-gradient-to-br from-blue-600 to-blue-800 flex flex-col gap-30 items-center justify-center">
 				<div className="text-white text-center space-y-4">
-					<h1 className="text-4xl font-bold">BDGAD System</h1>
-					<p className="text-xl">Admin Page</p>
+					<h1 className="text-5xl font-bold">BDGAD System</h1>
+					<p className="text-xl font-semibold">Admin Page</p>
 				</div>
+					<Image
+						src="/undraw_working_n9u0.svg"
+						alt="Working illustration"
+						width={800}
+						height={800}
+					/>
 			</div>
 
 			{/* Right side - Login form */}
